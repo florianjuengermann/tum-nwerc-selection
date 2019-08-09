995fc2b334380c6eedc8bad666516afab1ee68c9
Florian Jüngermann <florianjuengermann@gmail.com>
2019-08-09 14:26:57 +0200

    added scoring for codeforces

--- codeforcesContest.py
+++ codeforcesContest.py
@@ -1,23 +1,34 @@
+from contest import Contest
 import codeforcesApi as cfApi
 import re
 
 class CodeforcesContest(Contest):
 
-	def __init__(self, id):
-		handleString = "codeforces-handle"
-		super().__init__(self, id)
+	def __init__(self, id, handleMap):
+		self.handleString = "codeforces-handle"
+		super().__init__(id, handleMap)
 
-	def updateScores():
+	def updateScores(self):
+		print("fetching scores for CF contest ", self.id)
 		cfStandings = cfApi.request("contest.standings", {"contestId": self.id})
 		if cfStandings is False:
 			return
 		roundNr = re.search("[0-9][0-9]", cfStandings["contest"]["name"])
 		name = "Edu. " + str(roundNr)
 
-		rows = cfStandings["rows"]
+		# reset everything
+		self.handlesSolved = {}
+		self.numberSolved = {}
+		for i in range(len(cfStandings["problems"])):
+			self.numberSolved[i] = 0
 
-		self.allStandings = []
+		rows = cfStandings["rows"]
 		for r in rows:
 			handle = r["party"]["members"][0]["handle"]
-			for p in r["problemResults"]
-				solved =  p["points"] > 0
+			for i in range(len(r["problemResults"])):
+				solved =  r["problemResults"][i]["points"] > 0
+				if not handle in self.handlesSolved:
+					self.handlesSolved[handle] = []
+				if solved:
+					self.handlesSolved[handle].append(i)
+					self.numberSolved[i] += 1
--- contest.py
+++ contest.py
@@ -1,25 +1,46 @@
 from abc import ABC, abstractmethod
+import math
 
 class Contest(ABC):
 	"""abstract contest"""
 	id = "" 						# contest ID
 	name = ""
-	allStandings = None # [{"handle":…,"solved": <bool> }]
+	handlesSolved = None # {"handle": [<int>] } handle -> list of solved tasks
+	numberSolved = None	# {<int>: <int>} taskId -> number of people who solved it
 	handleString = "" 	# ∈ {"codefores-handle", "atcoder-handle"}
-	handleMap = None 		# [{"clear-name":…, "codforces-handle": …, "atc.h":…}]
+	handleMap = None 		# {"clear-name": {"codforces-handle": …, "atc.h":…} }
 	def __init__(self, id, handleMap):
 		self.handleMap
 		self.id = id
 
 	# clear name of user (not handle) -> score in this contest
-	def getScore(name: str) -> float:
-		if allStandings is None:
+	def getScore(self, name: str) -> float:
+		if self.handlesSolved is None:
 			self.updateScores()
-		return 1.0
+		#print(self.numberSolved)
+		handle = self.handleMap[name][handleString]
+		return self.calcHandleScore(handle)
 
+	def calcHandleScore(self, handle:str) -> float:
+		# number of total participants
+		participants = len(self.handlesSolved)
+		avgScore = 0
+
+		for nbSolved in numberSolved:
+			solvedFraction = nbSolved/participants
+			avgScore += solvedFraction * (1 - math.log(solvedFraction))
+		
+		# the unadjusted score for handle
+		score = 0
+		for taskI in handlesSolved[handle]:
+			solvedFraction = numberSolved[taskI]/participants
+			score += (1 - math.log(solvedFraction))
+
+		scaledScore = score / avgScore
+		return scaledScore
 		
 	# re-fetches all scores (may take a while)
 	# sets allStandings
 	@abstractmethod
-	def updateScores():
+	def updateScores(self):
 		pass
